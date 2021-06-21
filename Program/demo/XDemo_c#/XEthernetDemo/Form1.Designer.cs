0c17d9bf00c590a65e17ae44c111de733a9cb9aa
weike <weike@DESKTOP-J4CH4VH>
2021-06-21 11:09:56 +0800

    Revert "0617_2"
    This reverts commit 7f2f62a1631a5ba88ce00de752711c563e828547.

--- Program/demo/XDemo_c#/XEthernetDemo/Form1.Designer.cs
+++ Program/demo/XDemo_c#/XEthernetDemo/Form1.Designer.cs
@@ -1548,7 +1548,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 23);
             this.button1.TabIndex = 14;
-            this.button1.Text = "CheckTime";
+            this.button1.Text = "TestImage";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
