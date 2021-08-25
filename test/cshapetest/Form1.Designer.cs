7f8e45ab6c316db8f97ae3aeeddc8a7180e6253a
sixiyxy <38455360+sixiyxy@users.noreply.github.com>
2021-08-25 18:32:10 +0800

    Add files via upload
    8.25分应用解决线程冲突问题

---
+++ test/cshapetest/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace cshapetest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.testUdp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(282, 286);
+            this.start.Margin = new System.Windows.Forms.Padding(4);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(100, 29);
+            this.start.TabIndex = 2;
+            this.start.Text = "开始";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(452, 286);
+            this.Stop.Margin = new System.Windows.Forms.Padding(4);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(100, 29);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "暂停";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "数据处理方式：";
+            // 
+            // testUdp
+            // 
+            this.testUdp.Location = new System.Drawing.Point(626, 286);
+            this.testUdp.Margin = new System.Windows.Forms.Padding(4);
+            this.testUdp.Name = "testUdp";
+            this.testUdp.Size = new System.Drawing.Size(100, 29);
+            this.testUdp.TabIndex = 6;
+            this.testUdp.Text = "测试";
+            this.testUdp.UseVisualStyleBackColor = true;
+            this.testUdp.Click += new System.EventHandler(this.testUdp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 370);
+            this.Controls.Add(this.testUdp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.start);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button testUdp;
+    }
+}
+
